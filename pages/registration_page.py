4a2fe2ac3c1958b83d95072690831018ef3f5f6a
olegovnatim <olegovnatim@gmail.com>
2026-03-10 00:49:37 +0300

    изменение url

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -8,7 +8,7 @@ from data.users import User
 class RegistrationPage:
 
     def open(self):
-        browser.open('/automation-practice-form')
+        browser.open('https://demoqa.com/automation-practice-form')
         browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
             have.size_greater_than_or_equal(3)
         )
