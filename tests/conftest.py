3659325713ac15e569ada725ffdfaca3c2ad909d
olegovnatim <olegovnatim@gmail.com>
2026-03-10 00:43:06 +0300

    правки в conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,34 @@
 import pytest
-from selene.support.shared import browser
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# from utils import attach
 
 
-@pytest.fixture(scope="function", autouse=True)
-def open_browser():
-    browser.config.base_url = 'https://demoqa.com'
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": '128.0',
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
 
-    yield
+    yield driver
+    # attach.add_screenshot(driver)
+    # attach.add_page_source(driver)
+    # attach.add_console_logs(driver)
+    # attach.add_video(driver)
 
-    browser.quit()
+    driver.quit()
